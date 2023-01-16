7d22caddc54e170a784e6b9114cdf702fa9664bc
bitrevo <bitrevo@users.noreply.github.com>
2023-01-16 10:55:20 +0800

    remove debug code

--- retrick.php
+++ retrick.php
@@ -6,7 +6,7 @@
 </head>
 <body style="margin: 0px; background: #0e0e0e;text-align: center;">Redirecting...
 <br><a href="<?php echo str_replace("/", "#", htmlspecialchars($_GET["r"])); ?>"></a>
-<form id="a" method="post" action="<?php echo str_replace("/", "#", 'repost.php?i=' . htmlspecialchars($_GET["r"])); ?>">
+<form id="a" method="post" action="<?php echo str_replace("/", "#", htmlspecialchars($_GET["r"])); ?>">
 <!-- By posting dummy form to the image we can distinguish between direct traffic or link and show options after the upload -->
 	<input type="hidden" name="y" value="y">
 
